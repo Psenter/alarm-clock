8baab7b562f1ada2717137072c04ae49e24bcb1f
Parker S <parker.senter@gmail.com>
2023-06-01 16:44:17 +0000

    Added a popup message when the user has been on the site for 10 seconds.

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ setInterval(() => {
     let d = new Date ();
     time.innerHTML = d.toLocaleTimeString();
 },1000)
+
+setTimeout(function () { alert("You've been staring at the clock for 10 seconds now."); }, 10000); // 1000 = 1 second
