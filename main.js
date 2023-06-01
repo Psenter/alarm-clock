d2c582cfd0860ffe5d812071c0922a35969d8b2f
Parker S <parker.senter@gmail.com>
2023-06-01 17:15:33 +0000

    Added button, place to set timer, timer input, and inline styles, removed big-txt class.

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ setInterval(() => {
     time.innerHTML = d.toLocaleTimeString();
 },1000)
 
-setTimeout(function () { alert("You've been staring at the clock for 10 seconds now."); }, 10000); // 1000 = 1 second
+//setTimeout(function () { alert("You've been staring at the clock for 10 seconds now."); }, 10000); // 1000 = 1 second
